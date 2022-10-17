d7098c90271d43f4edb6b3e0280a6a8ddd571244
GiulioSnider <giuliosnider@gmail.com>
2022-10-17 13:32:57 +0200

    Aggiunta cartella Models e della Classe FIle Helper

---
+++ Models/FileHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+
+namespace Ex13_DronesAPI.Models
+{
+    public static class FileHelper
+    {
+        public static void SerializeAndWrite<T>(IEnumerable<T> itemList, string path)
+        {
+            var serializedList = JsonSerializer.Serialize(itemList);
+            File.WriteAllText(path, serializedList);
+        }
+
+        public static IEnumerable<T> ReadAndDesirializeFile<T>(string path)
+        {
+            var fileContent = File.ReadAllText(path);
+            if(fileContent == string.Empty)
+            {
+                return null;
+            }
+            return JsonSerializer.Deserialize<List<T>>(fileContent);
+        }
+    }
+}
