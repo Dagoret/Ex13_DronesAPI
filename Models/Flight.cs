5440b82a9c4d23b76d529e14f3229280a8755466
GiulioSnider <giuliosnider@gmail.com>
2022-10-17 13:56:41 +0200

    asdasd

--- Models/Flight.cs
+++ Models/Flight.cs
@@ -3,8 +3,8 @@
     public class Flight
     {
         public int FlightId { get; set; }
-        public DateOnly DepartureDate { get; set; }
-        public DateOnly ArrivalDate { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public DateTime ArrivalDate { get; set; }
         public int DroneId { get; set; }
     }
 }
