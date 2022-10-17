00d3393aa7f76c6e0e3c052d849ff08bc8bb05d9
oudeis <oudeis@OUDEIS>
2022-10-17 14:28:00 +0200

    c

---
+++ Controllers/FlightController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ex13_DronesAPI.Help;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Ex13_DronesAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FlightController : ControllerBase
+    {
+        // GET: api/<FlightController>
+        [HttpGet]
+        public IEnumerable<string> GetAllFlights()
+        {
+            return Ok();
+        }
+
+        // GET api/<FlightController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<FlightController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<FlightController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<FlightController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
