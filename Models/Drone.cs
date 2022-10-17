2cb55a18fd7ce3c154adf729aa74ef5d03030d97
GiulioSnider <giuliosnider@gmail.com>
2022-10-17 14:03:35 +0200

    Inserimento Enum

--- Models/Drone.cs
+++ Models/Drone.cs
@@ -1,5 +1,16 @@
 ﻿namespace Ex13_DronesAPI.Models
 {
+    public enum PropulsionTypes
+    {
+        Reaction = 1,
+        FixedWing = 2,
+        Helix = 3
+    }
+    public enum PilotTypes
+    {
+        Pilot = 0,
+        AI = 1
+    }
     public class Drone
     {
         public int DroneId { get; set; }
