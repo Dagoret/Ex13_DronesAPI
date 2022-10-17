66121f91b2cbeb3773b7a1ba9b8e030c89ce48c2
Francesco Fera <98550815+Francesco-Fera@users.noreply.github.com>
2022-10-17 14:14:00 +0200

    Created relative file paths

--- Help/Service.cs
+++ Help/Service.cs
@@ -2,5 +2,8 @@
 {
     public class Service
     {
+        private static string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        public static string DronePath { get; private set; } = $"{currentDirectory}..\\..\\..\\Files\\Drones.txt";
+        public static string FlightPath { get; private set; } = $"{currentDirectory}..\\..\\..\\Files\\Flights.txt";
     }
 }
